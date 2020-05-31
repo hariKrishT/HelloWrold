2b0789133110c88a239b85eb11ba857c9fd53d1b
Hari Krishna <harikrishnatippa@gmail.com>
2020-05-31 11:04:41 +0100

    add hello project

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO (Auto-generated method stub
+		myCall(2020);
+	}
+	static void myCall(int a){
+		System.out.print("Gnana welcome to Java world "+a);
+	}
+
+}
